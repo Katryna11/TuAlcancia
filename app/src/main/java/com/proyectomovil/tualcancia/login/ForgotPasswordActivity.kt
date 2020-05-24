92173107a56ddcb34008f07ac2dd380894e97f5b
Katherine Hernández <ingridhernandez_20182@corhuila.edu.co>
2020-05-24 16:26:07 -0500

    Login por correo, Registrarse y Restablecer contraseña  finalizados

--- app/src/main/java/com/proyectomovil/tualcancia/login/ForgotPasswordActivity.kt
+++ app/src/main/java/com/proyectomovil/tualcancia/login/ForgotPasswordActivity.kt
@@ -1,21 +1,45 @@
 package com.proyectomovil.tualcancia.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.proyectomovil.tualcancia.R
-import com.proyectomovil.tualcancia.goToActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.proyectomovil.tualcancia.*
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy {FirebaseAuth.getInstance()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
+        editTextEmail.validate{
+            editTextEmail.error = if(isValidEmail(it)) null else "Por favor ingrese un Email valido"
+        }
         buttonGoLogIn.setOnClickListener {
-            goToActivity<LoginActivity>()
+            goToActivity<LoginActivity>{
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
 
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+
+        buttonForgot.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            if(isValidEmail(email)){
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this){
+                    toast("El correo ha sido enviado para restablecer tu contraseña.")
+                    goToActivity<LoginActivity>{
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                }
+            }else{
+                toast("Por favor asegurate de que la dirección de correo es correcta.")
+
+            }
         }
     }
 }
